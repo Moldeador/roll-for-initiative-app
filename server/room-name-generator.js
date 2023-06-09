49edaad3d6203c0aa8cb33eabd19f16c6b3278a6
Santi <sontrax@gmail.com>
2023-06-09 12:06:09 +0200

    added room-name-generator.js

---
+++ server/room-name-generator.js
@@ -0,0 +1,35 @@
+const words = [
+	"dragon",
+	"vampire",
+	"wizard",
+	"ooze",
+	"lord",
+	"zombie",
+	"mage",
+	"goblin",
+	"knight",
+	"angel",
+	"elf",
+	"sword",
+	"shield",
+	"hoard",
+	"treasure",
+	"pole",
+	"manticore",
+	"orc",
+	"god",
+	"temple",
+	"ruins",
+	"thief",
+	"rogue",
+	"cleric",
+	"paladin",
+	"druid",
+	"sorcerer",
+	"demon",
+	"devil"
+];
+
+for (let index in words) {
+	console.log(words[index]);
+};
