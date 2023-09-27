cca664e79007b9024c99dad07940843dcc4cb16a
Santi <sontrax@gmail.com>
2023-09-27 21:33:50 +0200

    create_room.js changed address to be automatic

--- client/js/create_room.js
+++ client/js/create_room.js
@@ -1,5 +1,5 @@
 async function createNewRoom(){
-	let response = await fetch("http://localhost:3000/rooms", {method: "POST"});
+	let response = await fetch(`${location.protocol}//${location.hostname}/rooms`, {method: "POST"});
 	let data = await response.text();
 	location.href = `./room.html?name=${data}`;
 }
