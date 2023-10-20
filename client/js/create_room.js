aeabcbe88fe31db76197eb2d2578a78e4421a8ef
Santi <sontrax@gmail.com>
2023-10-20 10:39:19 +0200

    deleted client folder

--- client/js/create_room.js
+++
@@ -1,5 +0,0 @@
-async function createNewRoom(){
-	let response = await fetch(`${location.protocol}//${location.hostname}:3000/rooms`, {method: "POST"});
-	let data = await response.text();
-	location.href = `./room.html?name=${data}`;
-}
